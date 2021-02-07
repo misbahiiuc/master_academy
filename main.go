0df4b040b61d0b807e089c405e569df84d8cfb0a
misbahiiuc <misbahiiuc@gmail.com>
2021-02-07 14:44:39 +0600

    slice done

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main(){
 
-	students := [...]string{"Lutful", "Kader", "Misbah", "student", "CSE"}
-	fmt.Println(students)
-	fmt.Println(len(students))
+	//students := [...]string{"Lutful", "Kader", "Misbah", "student", "CSE"}
+	//fmt.Println(students)
+	//fmt.Println(len(students))
+	//x := students[0:2]
+	//fmt.Println(x)
+	//x := make([]string, 0)
+	//fmt.Println(x)
+	var fruits []string
+	//fmt.Println(fruits)
+	fruits = append(fruits, "Apple", "Banana")
+	//fmt.Println(fruits, len(fruits))
+	//fmt.Printf("%T\n", fruits)
+	//fmt.Printf("%T", students)
 	
+	b := reflect.TypeOf(fruits).Kind().String()
+	fmt.Println(b)
 	
-
-
 
 }
