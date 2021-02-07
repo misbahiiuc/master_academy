0b50f77568688551de532d76b5c8881aca2717b4
misbahiiuc <misbahiiuc@gmail.com>
2021-02-07 12:53:04 +0600

    initial slice commit

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import "fmt"
 
 func main(){
 
-	/*var students [3]string
-	fmt.Println(students)
-	fmt.Println(len(students))
-	students[0] = "Misbah"
-	students[1] = "Kader"
-	students[2] = "Lutful"
-	fmt.Println(students[0])*/
 	students := [...]string{"Lutful", "Kader", "Misbah", "student", "CSE"}
 	fmt.Println(students)
 	fmt.Println(len(students))
+	
 	
 
 
