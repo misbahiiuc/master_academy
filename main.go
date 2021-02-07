45b1e9c2c8d633248b7c7a9e9fb2c4267aeac068
misbahiiuc <misbahiiuc@gmail.com>
2021-02-07 12:44:42 +0600

    array data type

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	/*var students [3]string
+	fmt.Println(students)
+	fmt.Println(len(students))
+	students[0] = "Misbah"
+	students[1] = "Kader"
+	students[2] = "Lutful"
+	fmt.Println(students[0])*/
+	students := [...]string{"Lutful", "Kader", "Misbah", "student", "CSE"}
+	fmt.Println(students)
+	fmt.Println(len(students))
+	
+
+
+
+}
